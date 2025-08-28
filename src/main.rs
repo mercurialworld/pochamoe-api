968ceb42569efd79b660f8f2d11f02f26bf2ebdf
ポッチャマ <mercworld@pocha.moe>
2025-08-28 19:16:29 -0400

    feat: initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use axum::{
+    routing::get,
+    Router,
+};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use crate::routes::version::handler;
+
+mod routes;
+
+#[tokio::main]
+async fn main() {
+     tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().route("/v1/version/{mod_name}/{bs_version}", get(handler));
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:9669")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
---
+++ src/routes/version.rs
@@ -0,0 +1,143 @@
+use axum::{
+    debug_handler, extract::{FromRequestParts,  path::ErrorKind, rejection::PathRejection}, http::{StatusCode, request::Parts}, response::{IntoResponse, Response}
+};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
+use validator::{Validate, ValidationError};
+use regex::Regex;
+use std::sync::LazyLock;
+
+static RE_BS_VERSION: LazyLock<Regex> = LazyLock::new(|| {
+    Regex::new(r"[0-9]+\.[0-9]+\.[0-9]+(?:_[0-9]+)?$").unwrap()
+});
+
+#[derive(Debug, Deserialize, Validate)]
+pub struct ModParams {
+    #[validate(custom(function = "validate_mod", message = "Must be valid mod name"))]
+    pub mod_name: String,
+    #[validate(regex(path = *RE_BS_VERSION, message = "Must be valid Beat Saber version"))]
+    pub bs_version: String
+}
+
+#[debug_handler]
+// [TODO] do not hardcode this
+pub async fn handler(Path(params): Path<ModParams>) -> Result<&'static str, VersionError> {
+    match params.validate() {
+        Ok(_) => Ok("0.6.5.0"),
+        Err(e) => Err(VersionError(e.into())),
+    }
+}
+
+fn validate_mod(mod_name: &str) -> Result<(), ValidationError> {
+    tracing::debug!(mod_name);
+    // [TODO] do not hardcode this
+    if mod_name.eq_ignore_ascii_case("dumbrequestmanager") {
+        return Ok(());
+    }
+
+    Err(ValidationError::new("Invalid mod name"))
+}
+
+pub struct VersionError(anyhow::Error);
+
+impl IntoResponse for VersionError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::BAD_REQUEST,
+            format!("{}", self.0),
+        )
+            .into_response()
+    }
+}
+
+pub struct Path<T>(T);
+
+impl<S, T> FromRequestParts<S> for Path<T>
+where
+    T: DeserializeOwned + Send,
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, axum::Json<PathError>);
+
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        match axum::extract::Path::<T>::from_request_parts(parts, state).await {
+            Ok(value) => Ok(Self(value.0)),
+            Err(rejection) => {
+                let (status, body) = match rejection {
+                    PathRejection::FailedToDeserializePathParams(inner) => {
+                        let mut status = StatusCode::BAD_REQUEST;
+
+                        let kind = inner.into_kind();
+                        let body = match &kind {
+                            ErrorKind::WrongNumberOfParameters { .. } => PathError {
+                                message: kind.to_string(),
+                                location: None,
+                            },
+
+                            ErrorKind::ParseErrorAtKey { key, .. } => PathError {
+                                message: kind.to_string(),
+                                location: Some(key.clone()),
+                            },
+
+                            ErrorKind::ParseErrorAtIndex { index, .. } => PathError {
+                                message: kind.to_string(),
+                                location: Some(index.to_string()),
+                            },
+
+                            ErrorKind::ParseError { .. } => PathError {
+                                message: kind.to_string(),
+                                location: None,
+                            },
+
+                            ErrorKind::InvalidUtf8InPathParam { key } => PathError {
+                                message: kind.to_string(),
+                                location: Some(key.clone()),
+                            },
+
+                            ErrorKind::UnsupportedType { .. } => {
+                                status = StatusCode::INTERNAL_SERVER_ERROR;
+                                PathError {
+                                    message: kind.to_string(),
+                                    location: None,
+                                }
+                            }
+
+                            ErrorKind::Message(msg) => PathError {
+                                message: msg.clone(),
+                                location: None,
+                            },
+
+                            _ => PathError {
+                                message: format!("Unhandled deserialization error: {kind}"),
+                                location: None,
+                            },
+                        };
+
+                        (status, body)
+                    }
+                    PathRejection::MissingPathParams(error) => (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        PathError {
+                            message: error.to_string(),
+                            location: None,
+                        },
+                    ),
+                    _ => (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        PathError {
+                            message: format!("Unhandled path rejection: {rejection}"),
+                            location: None,
+                        },
+                    ),
+                };
+
+                Err((status, axum::Json(body)))
+            }
+        }
+    }
+}
+
+#[derive(Serialize)]
+pub struct PathError {
+    message: String,
+    location: Option<String>,
+}
